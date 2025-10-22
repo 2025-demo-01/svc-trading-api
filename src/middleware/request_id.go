5faba5c3d48b28221e61626073ed640d8029d1bf
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-22 16:12:51 +0900

    Create request_id.go

---
+++ src/middleware/request_id.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type ctxKey string
+
+const requestIDKey ctxKey = "reqid"
+
+func WithRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := r.Header.Get("X-Request-Id")
+		if id == "" {
+			id = uuid.NewString()
+		}
+		ctx := context.WithValue(r.Context(), requestIDKey, id)
+		w.Header().Set("X-Request-Id", id)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
