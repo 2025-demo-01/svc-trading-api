0091ccaf3c6e34e3d7ee3703ba2084e8bb4e8c44
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-22 16:10:34 +0900

    Update router.go

--- src/router.go
+++ src/router.go
@@ -8,8 +8,14 @@ import (
 
 func setupRouter() http.Handler {
 	r := mux.NewRouter()
+
+	// Health/Ready/Metrics
 	r.HandleFunc("/healthz", Health).Methods("GET")
+	r.HandleFunc("/readyz", Ready).Methods("GET") // [ADDED]
 	r.Handle("/metrics", Metrics())
+
+	// Business endpoints (stub)
 	r.HandleFunc("/api/v1/trade/orders", CreateOrder).Methods("POST")
+
 	return r
 }
